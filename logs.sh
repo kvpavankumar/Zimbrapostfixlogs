a44b5fe0f12a378001bd752b4299ac9c4f8a2a9c
Pavan Kumar <pavannani@ymail.com>
2023-04-28 10:40:54 +0530

    Update logs.sh

--- logs.sh
+++ logs.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 sudo curl https://raw.githubusercontent.com/kvpavankumar/Zimbrapostfixlogs/master/50-default.conf > /etc/rsyslog.d/50-default.conf
 sudo curl https://raw.githubusercontent.com/kvpavankumar/Zimbrapostfixlogs/master/rsyslog> /etc/logrotate.d/rsyslog 
 sudo curl https://raw.githubusercontent.com/kvpavankumar/Zimbrapostfixlogs/master/rsyslog.conf>/etc/rsyslog.conf
