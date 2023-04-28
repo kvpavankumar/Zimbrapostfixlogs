2a61cdd197186a216b169ac58fd35bfe72c4d713
Pavan Kumar <pavannani@ymail.com>
2023-04-28 10:44:16 +0530

    Update logs.sh

--- logs.sh
+++ logs.sh
@@ -3,5 +3,5 @@ sudo curl https://raw.githubusercontent.com/kvpavankumar/Zimbrapostfixlogs/maste
 sudo curl https://raw.githubusercontent.com/kvpavankumar/Zimbrapostfixlogs/master/rsyslog> /etc/logrotate.d/rsyslog 
 sudo curl https://raw.githubusercontent.com/kvpavankumar/Zimbrapostfixlogs/master/rsyslog.conf>/etc/rsyslog.conf
 sudo systemctl restart syslog.service
-sudo curl https://raw.githubusercontent.com/kvpavankumar/Zimbrapostfixlogs/master/zimbra_pflogsumm.pl /opt/zimbra/bin/zimbra_pflogsumm.pl
+sudo curl https://raw.githubusercontent.com/kvpavankumar/Zimbrapostfixlogs/master/zimbra_pflogsumm.pl >/opt/zimbra/bin/zimbra_pflogsumm.pl
 
